6a3f2c7d08fcf7c058f947822e21e430f8867efa
hawon6691 <hawonoj@outlook.kr>
2026-02-11 11:40:18 +0900

    feat: (#41) 관심 상품 등록 구현

--- app/src/routes/product.js
+++ app/src/routes/product.js
@@ -22,6 +22,11 @@ const ProductTagRepository = require("./../productTags/productTag.repository");
 
 const SearchRepository = require("./../search/search.repository");
 
+const LikedController = require("./../liked/liked.controller");
+const LikedService = require("./../liked/liked.service");
+const LikedRepository = require("./../liked/liked.repository");
+const { likeProductValidator } = require("../validators/liked.validator");
+
 const router = express.Router();
 
 const tagRepository = new TagRepository();
@@ -46,6 +51,10 @@ const productService = new ProductService(
 );
 const productController = new ProductController(productService);
 
+const likedRepository = new LikedRepository();
+const likedService = new LikedService(likedRepository, productRepository);
+const likedController = new LikedController(likedService);
+
 router.post(
   "/",
   authGuard(),
@@ -56,6 +65,11 @@ router.post(
 );
 
 router.get("/trending", productController.findTrendingProducts);
+
+router.post("/liked", authGuard(), likeProductValidator, validate, likedController.likeProduct);
+router.delete("/liked", authGuard(), likeProductValidator, validate, likedController.unlikeProduct);
+router.get("/liked", authGuard(), likedController.getLikedProducts);
+
 router.get("/", productController.findProducts);
 router.get("/:id", productController.findProductById);
 
